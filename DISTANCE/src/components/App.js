fe02ba37361c39e4b6d9b51201960310d6791f53
MorozMichal <34334015+MorozMichal@users.noreply.github.com>
2019-03-29 13:40:29 +0100

    create calculator distance

---
+++ DISTANCE/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>kalkulator dystansu</h1>
+      </>
+    );
+  }
+}
+
+export default App;

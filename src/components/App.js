8d8bf76c053308cbe9d90eb21c0b3ed8100ef494
MorozMichal <34334015+MorozMichal@users.noreply.github.com>
2019-03-29 12:58:13 +0100

    end script logic calculator BMI

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 const Result = (props) => {
-  const result = (props.wzrost / props.waga).toFixed(2)
-  if (props.wzrost <= 0 || props.waga <= 0) {
-    return "Podaj niezbędne dane"
+  const result = (props.waga / (((props.wzrost / 100) * props.wzrost) / 100)).toFixed(2)
+  if (props.waga > 0 && props.wzrost > 0) {
+    if (result < 16.0) {
+      return result;
+    } else if (result <= 16.99) {
+      return `${result} - WYCHUDZENIE`;
+    } else if (result <= 18.49) {
+      return `${result} - NIEDOWAGA`;
+    } else if (result <= 24.99) {
+      return `${result} - PRAWIDŁOWA WAGA`;
+    } else if (result <= 29.99) {
+      return `${result}- NADWAGA`;
+    } else if (result <= 34.99) {
+      return `${result} - I STOPIEŃ OTYŁOŚCI`;
+    } else if (result <= 39.99) {
+      return `${result} - II STOPIEŃ OTYŁOŚCI`;
+    } else {
+      return `${result} - III STOPIEŃ OTYŁOŚCI`;
+    }
   } else {
-    return result
+    return "PROSZĘ PODAĆ PRAWIDŁOWE DANE";
   }
-
 }
 
 class App extends Component {
@@ -29,6 +44,13 @@ class App extends Component {
     })
   }
 
+  handleReset = () => {
+    this.setState({
+      waga: "",
+      wzrost: ""
+    })
+  }
+
   render() {
     const { wzrost, waga } = this.state
     return (
@@ -38,6 +60,7 @@ class App extends Component {
         <input id="wzrost" value={wzrost} onChange={this.handleWzrost} type="text" placeholder="wpisz dane"></input>
         <label htmlFor="waga">Podaj wagę w kg</label>
         <input id="waga" value={waga} onChange={this.handleWaga} type="text" placeholder="wpisz dane"></input>
+        <button onClick={this.handleReset}>Wyczyść dane</button>
         <p className="info">Przy wzroście {wzrost} oraz wadze {waga} Twój współczynnik BMI wynosi:</p>      <p className="result"><Result wzrost={wzrost} waga={waga} /></p>
       </div>
     )
